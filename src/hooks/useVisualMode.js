f195542780e969ef4788f9aa8b7c6c670e01753f
Mitchell Stave <mitchjohnston99@gmail.com>
2023-11-07 16:37:34 -0800

    added constraints to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); 
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode])
+  function transition(newMode, replace=false){
+    setHistory(prev => replace ? [...prev.slice(0, prev.length - 1)].concat(newMode): [...prev, newMode])
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    setHistory(prev => prev.length === 1 ? prev : [...prev.slice(0, prev.length - 1)])
   }
 
   return { mode: history[history.length -1], transition, back };
