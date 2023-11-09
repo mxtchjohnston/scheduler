76cdd5540a1e412b07d1a955f83c0ec0f7c95f52
mxtchjohnston <mitchjohnston99@gmail.com>
2023-11-08 21:47:17 -0800

    added cypress

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,51 +4,51 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day };
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      };
-    case SET_INTERVIEW: {
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: action.interview && { ...action.interview }
-      };
+  case SET_DAY:
+    return { ...state, day: action.day };
+  case SET_APPLICATION_DATA:
+    return {
+      ...state,
+      days: action.days,
+      appointments: action.appointments,
+      interviewers: action.interviewers
+    };
+  case SET_INTERVIEW: {
+    const appointment = {
+      ...state.appointments[action.id],
+      interview: action.interview && { ...action.interview }
+    };
 
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment
-      };
+    const appointments = {
+      ...state.appointments,
+      [action.id]: appointment
+    };
 
-      const getSpotsForDay = day =>
-        day.appointments.length -
+    const getSpotsForDay = day =>
+      day.appointments.length -
         day.appointments.reduce(
           (count, id) => (appointments[id].interview ? count + 1 : count),
           0
         );
 
-      const days = state.days.map(day => {
-        return day.appointments.includes(action.id)
-          ? {
-              ...day,
-              spots: getSpotsForDay(day)
-            }
-          : day;
-      });
+    const days = state.days.map(day => {
+      return day.appointments.includes(action.id)
+        ? {
+          ...day,
+          spots: getSpotsForDay(day)
+        }
+        : day;
+    });
 
-      return {
-        ...state,
-        appointments,
-        days
-      };
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
+    return {
+      ...state,
+      appointments,
+      days
+    };
+  }
+  default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
   }
 }
