ef7e776c7cb3e6abae5e0bc4e99cfcab774e62a4
mxtchjohnston <mitchjohnston99@gmail.com>
2023-11-09 19:56:50 -0800

    added cypress e2e tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,12 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("Should navigate to tuesday", () => {
+    cy.get("li").contains("Tuesday").click();
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
 });
